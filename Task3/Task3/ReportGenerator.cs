30b6c1b79b11522d12dde7dd624027d15c9ef04b
deniszadorog1 <deniszadorog15@gmail.com>
2025-06-12 11:40:22 +0300

    Base none SOLID ReportGenerator

---
+++ Task3/Task3/ReportGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace Task3
+{
+    internal class ReportGenerator
+    {
+        private readonly DatabaseService _db = new DatabaseService();
+        private readonly FileLogger _logger = new FileLogger();
+
+        public void Generate()
+        {
+            var data = _db.GetData();
+            _logger.Log("Report created.");
+        }
+    }
+}
